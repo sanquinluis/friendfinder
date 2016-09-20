77a5e7868142164fab43ad516899c77f27a7770e
Hector L Santana-Quintana <sanquinluis@hotmail.com>
2016-09-20 16:32:25 -0400

    Adding my json file and updating the npm

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+// Dependencies 
+// =============================================================
 var express = require('express');
 var bodyParser = require('body-parser');
 const path = require('path');
@@ -12,4 +14,4 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
-
+// =============================================================
